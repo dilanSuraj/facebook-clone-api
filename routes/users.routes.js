446c538d81b421eb48abc789d3b7a84cea765cee
Dilan Amarasinghe <it17121002@my.sliit.lk>
2020-09-19 17:38:24 +0530

    bug fixed

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 /**
  * Synchronized method
  */
-router.post('/', (req, res) => {
+router.post('/',async (req, res) => {
     const user = new UserModel({
         surname: req.body.surname,
         user_name: req.body.user_name,
@@ -14,27 +14,30 @@ router.post('/', (req, res) => {
         gender: req.body.gender,
         birth_date: req.body.birth_date
     });
-    const user = await UserModel.find({
+    const user_existing = await UserModel.find({
         user_name: req.body.user_name
     });
-    if(user !== null){
+    if (user_existing !== null) {
         res.json({
             status: 304,
             message: "User exist"
         });
     }
-    user.save().then((data) => {
-        res.json({
-            status: 200,
-            message: data
-        });
-    }).catch(err => {
-        res.json({
-            status: err.code,
-            message: err
-        });
-        console.log(`Error : ${err}`);
-    })
+    else {
+        user.save().then((data) => {
+            res.json({
+                status: 200,
+                message: data
+            });
+        }).catch(err => {
+            res.json({
+                status: err.code,
+                message: err
+            });
+            console.log(`Error : ${err}`);
+        })
+    }
+
 });
 
 
