504b9488dd62de2a567e295783019c8b6a7138b9
Dilan Amarasinghe <it17121002@my.sliit.lk>
2020-09-19 18:20:59 +0530

    bugs fixed

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const UserModel = require('../models/User');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 /**
  * Synchronized method
  */
-router.post('/',async (req, res) => {
+router.post('/', async (req, res) => {
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(req.body.hashed_password, salt);
     const user = new UserModel({
         surname: req.body.surname,
         user_name: req.body.user_name,
-        hashed_password: req.body.hashed_password,
+        hashed_password: hash,
         first_name: req.body.first_name,
         gender: req.body.gender,
         birth_date: req.body.birth_date
@@ -17,7 +21,7 @@ router.post('/',async (req, res) => {
     const user_existing = await UserModel.find({
         user_name: req.body.user_name
     });
-    
+
     if (user_existing.length !== 0) {
         res.status(304).json({
             code: 304
@@ -38,11 +42,21 @@ router.post('/',async (req, res) => {
 router.get('/:username/:pw', async (req, res) => {
 
     try {
+        
         const user = await UserModel.find({
-            user_name: req.params.username,
-            hashed_password: req.params.pw
+            user_name: req.params.username
         });
-        res.status(200).json(user);
+        if(user.length == 0){
+            res.status(500).json("User not found");
+        }
+        let isPasswordMatch = bcrypt.compareSync(req.params.pw, user[0].hashed_password);
+        if(isPasswordMatch){
+            res.status(200).json(user);
+        }
+        else{
+            res.status(500).json("Password is incorrect");
+        }
+        
     } catch (err) {
         console.log(err)
         res.status(500).json("Server error");
